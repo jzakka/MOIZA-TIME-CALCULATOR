d13167873165cf9b10c6bc05310ecc83292a6d1d
Jzakka <mouse4786@gmail.com>
2023-07-03 14:45:09 +0900

    Refactor: 불필요한 메서드 삭제

--- src/main/java/com/ll/moizatimecalculator/boundedContext/room/repository/EnterRoomRepository.java
+++ src/main/java/com/ll/moizatimecalculator/boundedContext/room/repository/EnterRoomRepository.java
@@ -7,13 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EnterRoomRepository extends JpaRepository<EnterRoom, Long> {
-    Optional<EnterRoom> findByRoomAndMember(Room room, Member member);
-
     @Query(value = "select er.member from EnterRoom as er where er.room = ?1 order by er.member")
     List<Member> findMembersByRoom(Room room);
-
-    Optional<EnterRoom> findByMemberIdAndRoomId(Long memberId, Long roomId);
 }
